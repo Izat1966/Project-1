3dca57d32a6a970bc31c154ddf35e009331727c3
Izat1966 <150505243+Izat1966@users.noreply.github.com>
2023-12-06 19:49:27 +0600

    Update project 1.cpp

--- project 1.cpp
+++ project 1.cpp
@@ -5,21 +5,31 @@
 Write your code in this editor and press "Run" button to compile and execute it.
 
 *******************************************************************************/
-
 #include <iostream>
-#include <algorithm>
 #include <random>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
 int main() {
-    int n = 10, a, b, cnt = 0, moves = 0;
-    int arr[n] = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5};
-    int found[n] = {0};
-    
+    int n,a,b,cnt = 0, moves = 0;
+    int minelement=6;
+    int maxelement=20;
+    cout<<"Enter the size of an array (6-20)"<<endl;
+    cin>>n;
+    if (n<minelement || n>maxelement) {
+        cout << "Invalid array size. Please enter a value between 6 and 20" << endl;
+        return 1;
+    }
+    vector<int> arr(n);
+    vector<int> found(n, 0);
+
+    arr={1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10};
+    arr.resize(n);
     random_device rd;
     default_random_engine rng(rd());
-    shuffle(arr, arr + n, rng);
-    
+    shuffle(arr.begin(), arr.end(), rng);
+
     while (true) {
         for (int i = 0; i < n; i++) {
             if (found[i] == 0) {
@@ -33,18 +43,22 @@ int main() {
         cin >> a >> b;
         cout << "The number at index " << a << " is " << arr[a] << endl;
         cout << "The number at index " << b << " is " << arr[b] << endl;
-        if (arr[a] == arr[b] && a!=b) {
+        if (arr[a] == arr[b] && a!=b && a>=0 && b>=0) {
             cout << "These numbers are equal" << endl;
             found[a] = 1;
             found[b] = 1;
             cnt += 2;
-        } else {
+        } else if (a==b) {
+            cout << "The indices are two same numbers , they must be different" << endl;
+        }else if (a<0 || b<0) {
+            cout << "Invalid indices"<<endl;
+        }else {
             cout << "These numbers are not equal" << endl;
         }
-        moves++; 
+        moves++;
         if (cnt == n) {
             cout << "Great job! You found " << moves << " moves" << endl;
-            break; 
+            break;
         }
     }
     return 0;
